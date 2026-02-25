6e68f7069e868a00cc055759f77e9a598d1d4406
Root <infra@cronapp.io>
2026-02-25 13:06:55 +0000

    ghcvbbc

--- src/main/java/blockly/Application.java
+++ src/main/java/blockly/Application.java
@@ -5,26 +5,33 @@ import cronapi.rest.security.CronappSecurity;
 import java.util.concurrent.Callable;
 
 
-
 @CronapiMetaData(type = "blockly")
 @CronappSecurity
 public class Application {
 
-  public static final int TIMEOUT = 300;
+public static final int TIMEOUT = 300;
 
-  /**
-   *
-   * @return Var
-   */
-  // Application
-  public static Var Current() throws Exception {
-    return new Callable<Var>() {
+/**
+ *
+ * Application
+ *
+ * @author Root
+ * @since 25/02/2026, 10:07:17
+ *
+ */
+public static Var Current() throws Exception {
+ return new Callable<Var>() {
 
-      public Var call() throws Exception {
-        return
-            cronapi.util.Operations.getApplicationId();
-      }
-    }.call();
-  }
+   private Var item = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    item = cronapi.logic.Operations.isEmpty(
+    Var.valueOf("test"));
+    return
+cronapi.util.Operations.getApplication();
+   }
+ }.call();
+}
 
 }
+
